bdec9f61f808336ab9485b164d6a08e6e9a108e0
rrirr <irisalvz@gmail.com>
2020-02-11 00:34:01 +0100

    comentando

--- srclib/sockets.c
+++ srclib/sockets.c
@@ -1,26 +1,35 @@
 #include "sockets.h"
 
+/*
+* Funcion que crea un socket TCP/IP, lo liga al puerto SERVER_PORT y lo
+* deja escuchando.
+*/
 int socket_server_ini()
 {
   int sockval;
   struct sockaddr_in addr;
 
+  /* Se crea el socket*/
   syslog (LOG_INFO, "Creating socket");
   if ((sockval = socket(AF_INET, SOCK_STREAM, 0)) < 0){
     syslog(LOG_ERR, "Error creating socket");
     exit(EXIT_FAILURE);
   }
+
+  /* Se inicializa la estructura addr*/
   addr.sin_family = AF_INET;                /* TCP/IP family */
   addr.sin_port = htons(SERVER_PORT);   /* Asigning port */
   addr.sin_addr.s_addr = htonl(INADDR_ANY); /* Accept all adresses */
   bzero((void *) &(addr.sin_zero), 8);
 
+  /* Se liga el puerto al socket */
   syslog (LOG_INFO, "Binding socket");
   if (bind(sockval, (struct sockaddr *)&addr, sizeof(addr)) < 0){
     syslog(LOG_ERR, "Error binding socket");
     exit(EXIT_FAILURE);
   }
 
+  /* Se comienza a escuchar */
   syslog (LOG_INFO, "Listening connections");
   if (listen(sockval, MAX_CONNECTIONS) < 0){
     syslog(LOG_ERR, "Error listenining");
@@ -29,6 +38,9 @@ int socket_server_ini()
   return sockval;
 }
 
+/*
+* Funcion que acepta la conexion de un cliente.
+*/
 void socket_accept(int sockval)
 {
   int connval, len;
