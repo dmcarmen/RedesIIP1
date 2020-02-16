de09c9f41c6c93f3028915110eacfeff83e75a2c
rrirr <irisalvz@gmail.com>
2020-02-17 00:57:24 +0100

    pequearreglo

--- main.c
+++ main.c
@@ -11,15 +11,23 @@ void manejador(int sig) {
 int main(int argc, char const *argv[]) {
   int sockval;
   pool_thread * pool;
+  struct sigaction act;
 
-  signal(SIGINT, manejador);
+  act.sa_handler = manejador;
+  sigemptyset(&(act.sa_mask));
+  act.sa_flags = 0;
+
+  if (sigaction(SIGINT, &act, NULL) < 0) {
+    syslog(LOG_ERR, "Error sigaction");
+    return 0;
+  }
 
   sockval = socket_server_ini();
 
   pool = pool_create(sockval);
 
   while(flag);
-  
+
   pool_free(pool);
   close(sockval);
   return 0;
